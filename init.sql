05b887ae983f4eb72fec5b6abd0969f598e6bdb1
KaayTx <kawtar.dahman@student.vinci.be>
2024-02-25 21:13:04 +0100

    feat: add init.sql

---
+++ init.sql
@@ -0,0 +1,19 @@
+DROP SCHEMA IF EXISTS pae CASCADE;
+CREATE SCHEMA pae;
+
+DROP TYPE IF EXISTS roles;
+CREATE TYPE roles AS ENUM ('étudiant', 'prof', 'administrateur');
+
+CREATE TABLE pae.utilisateurs(
+                                 id_utilisateur SERIAL PRIMARY KEY,
+                                 email VARCHAR(30) NOT NULL,
+                                 mot_de_passe VARCHAR(60) NOT NULL,
+                                 nom VARCHAR(50) NOT NULL,
+                                 prenom VARCHAR(50) NOT NULL,
+                                 telephone CHAR(13),
+                                 date_inscription DATE NOT NULL,
+                                 role roles NOT NULL
+);
+
+
+INSERT INTO pae.utilisateurs (email, mot_de_passe, nom, prenom, telephone, date_inscription, role) VALUES ('chuqi.chups@student.vinci.be', 'Azertyui1_', 'chuqi', 'chups', '04 666 666 66', CURRENT_DATE, 'étudiant');
