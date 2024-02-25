ec4357f773e959502b62b6d4376c2f459c259e5b
KaayTx <kawtar.dahman@student.vinci.be>
2024-02-25 21:08:19 +0100

    feat: completing DTO for user with documentation

--- src/main/java/be/vinci/pae/business/domain/User.java
+++ src/main/java/be/vinci/pae/business/domain/User.java
@@ -1,21 +1,8 @@
 package be.vinci.pae.business.domain;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-@JsonDeserialize(as = UserImpl.class)
-public interface User implements UserDTO {
+public interface User extends UserDTO {
 
-  String getLogin();
-
-  void setLogin(String login);
-
-  int getId();
-
-  void setId(int id);
-
-  String getPassword();
-
-  void setPassword(String password);
 
   boolean checkPassword(String password);
 
--- src/main/java/be/vinci/pae/business/domain/UserDTO.java
+++ src/main/java/be/vinci/pae/business/domain/UserDTO.java
@@ -1,5 +1,51 @@
 package be.vinci.pae.business.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = UserImpl.class)
 public interface UserDTO {
+
+  /**
+   * Returns the user's id
+   *
+   * @return the user's id
+   */
+  int getId();
+
+  /**
+   * Sets the user's id
+   *
+   * @param id the user's id to set
+   */
+  void setId(int id);
+
+  /**
+   * Returns the user's email
+   *
+   * @return the user's email
+   */
+  String getEmail();
+
+  /**
+   * Sets the user's email
+   *
+   * @param email the user's email to set
+   */
+  void setEmail(String email);
+
+
+  /**
+   * Returns the user's password
+   *
+   * @return the user's password
+   */
+  String getPassword();
+
+  /**
+   * Sets the user's password
+   *
+   * @param password the user's password to set
+   */
+  void setPassword(String password);
 
 }
