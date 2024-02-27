7a11c17af0e682fb1ded0cc073fb8b331b3ef65c
Guillaume Kennes <guillaume.kennes@student.vinci.be>
2024-02-27 14:42:05 +0100

    fix: change db in english

--- init.sql
+++ init.sql
@@ -1,16 +1,16 @@
 DROP SCHEMA IF EXISTS pae CASCADE;
 CREATE SCHEMA pae;
 
-CREATE TABLE pae.utilisateurs(
-                                 id_utilisateur SERIAL PRIMARY KEY,
+CREATE TABLE pae.users(
+                                 id_user SERIAL PRIMARY KEY,
                                  email VARCHAR(30) NOT NULL,
-                                 mot_de_passe VARCHAR(60) NOT NULL,
-                                 nom VARCHAR(50) NOT NULL,
-                                 prenom VARCHAR(50) NOT NULL,
-                                 telephone CHAR(13),
-                                 date_inscription DATE NOT NULL,
-                                 role NOT NULL IN ('étudiant', 'professeur', 'administratif');
+                                 password VARCHAR(60) NOT NULL,
+                                 last_name VARCHAR(50) NOT NULL,
+                                 first_name VARCHAR(50) NOT NULL,
+                                 phone_number CHAR(13),
+                                 registration_date DATE NOT NULL,
+                                 role VARCHAR(15) NOT NULL
 );
 
 
-INSERT INTO pae.utilisateurs (email, mot_de_passe, nom, prenom, telephone, date_inscription, role) VALUES ('chuqi.chups@student.vinci.be', 'Azertyui1_', 'chuqi', 'chups', '04 666 666 66', CURRENT_DATE, 'étudiant');
+INSERT INTO pae.users (email, password, last_name, first_name, phone_number, registration_date, role) VALUES ('chuqi.chups@student.vinci.be', 'Azertyui1_', 'chuqi', 'chups', '04 666 666 66', CURRENT_DATE, 'étudiant');
