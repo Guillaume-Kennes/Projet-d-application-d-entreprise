ccf1a256a47a1273ed1ee38aaa2c68f8eead5637
Guillaume Kennes <guillaume.kennes@student.vinci.be>
2024-03-04 00:50:57 +0100

    fix: rollback from the previous commit

--- src/main/java/be/vinci/pae/business/ucc/UserUCCImpl.java
+++ src/main/java/be/vinci/pae/business/ucc/UserUCCImpl.java
@@ -4,6 +4,8 @@ import be.vinci.pae.business.domain.User;
 import be.vinci.pae.business.domain.UserDTO;
 import be.vinci.pae.dal.UserDAO;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response.Status;
 
 public class UserUCCImpl implements UserUCC {
 
@@ -17,7 +19,15 @@ public class UserUCCImpl implements UserUCC {
    */
   public UserDTO login(String email, String password) {
     User userFound = (User) userDAO.getUserByEmail(email);
-    System.out.println("USER " + userFound);
+    System.out.println("USER (from UserUCCImpl) " + userFound);
+    // need token before this works
+    if (userFound == null) {
+      throw new WebApplicationException("Incorrect Email or Password", Status.UNAUTHORIZED);
+    }
+
+    if (!userFound.checkPassword(password)) {
+      throw new WebApplicationException("Incorrect Email or Password", Status.UNAUTHORIZED);
+    }
 
     return userFound;
   }
