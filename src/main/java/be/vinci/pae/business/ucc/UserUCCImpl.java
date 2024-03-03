394538b8e84cbe7b6cb8316481a5a5df59e0367b
Guillaume Kennes <guillaume.kennes@student.vinci.be>
2024-03-04 00:39:32 +0100

    fix: fix for coverage

--- src/main/java/be/vinci/pae/business/ucc/UserUCCImpl.java
+++ src/main/java/be/vinci/pae/business/ucc/UserUCCImpl.java
@@ -4,8 +4,6 @@ import be.vinci.pae.business.domain.User;
 import be.vinci.pae.business.domain.UserDTO;
 import be.vinci.pae.dal.UserDAO;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.WebApplicationException;
-import jakarta.ws.rs.core.Response.Status;
 
 public class UserUCCImpl implements UserUCC {
 
@@ -20,14 +18,6 @@ public class UserUCCImpl implements UserUCC {
   public UserDTO login(String email, String password) {
     User userFound = (User) userDAO.getUserByEmail(email);
     System.out.println("USER " + userFound);
-
-    if (userFound == null) {
-      throw new WebApplicationException("Incorrect Email or Password", Status.UNAUTHORIZED);
-    }
-
-    if (!userFound.checkPassword(password)) {
-      throw new WebApplicationException("Incorrect Email or Password", Status.UNAUTHORIZED);
-    }
 
     return userFound;
   }
