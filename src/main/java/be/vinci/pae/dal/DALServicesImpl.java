f92f0ac305dc0fac3a9d54c68e8bf2144a6cc16c
mona-naitmazi <mona.naitmazi@student.vinci.be>
2024-03-14 13:28:38 +0100

    refactor: small changes

--- src/main/java/be/vinci/pae/dal/DALServicesImpl.java
+++ src/main/java/be/vinci/pae/dal/DALServicesImpl.java
@@ -2,6 +2,7 @@ package be.vinci.pae.dal;
 
 import be.vinci.pae.utils.Config;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -12,23 +13,19 @@ import java.io.InputStream;
 
 public class DALServicesImpl implements DALBackServices, DALServices {
 
-<<<<<<< HEAD
-
-  private Connection connection;
-
+  //private Connection connection;
+/*
   public DALServicesImpl() {
     try {
       connection = DriverManager.getConnection(Config.getProperty("DatabaseFilePath"), Config.getProperty("DatabaseUser"), Config.getProperty("DatabasePassword"));
     } catch (SQLException e){
       throw new RuntimeException("Unable to connect to database" + e.getMessage());
     }
-  }
-=======
+  }*/
   private ThreadLocal<Connection> connections;
   private BasicDataSource connectionPool;
 
   public DALServicesImpl() {
->>>>>>> e72932560d3b7411c173a0c523f7189bc60001a6
 
     Properties properties = new Properties();
     try (InputStream input = new FileInputStream("dev.properties")) {
