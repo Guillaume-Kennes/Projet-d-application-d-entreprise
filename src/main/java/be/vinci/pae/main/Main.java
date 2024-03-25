f4da98f134e7f9e35b6857eafae4fb617251c171
KaayTx <kawtar.dahman@student.vinci.be>
2024-03-25 01:29:25 +0100

    feat: add javadoc to pass Jenkins

--- src/main/java/be/vinci/pae/main/Main.java
+++ src/main/java/be/vinci/pae/main/Main.java
@@ -13,6 +13,12 @@ import org.glassfish.jersey.server.ResourceConfig;
  * Main class.
  */
 public class Main {
+
+  /**
+   * Retrieves the base URI for the application.
+   *
+   * @return The base URI retrieved from the configuration properties.
+   */
   public static final String BASE_URI = Config.getProperty("BaseUri");
 
 
