b438620a456652bd632113a5b93db1d3b574721f
mona-naitmazi <mona.naitmazi@student.vinci.be>
2024-03-25 17:51:57 +0100

    i want to merge

--- src/test/java/be/vinci/pae/ucc/ViewContactUCCTest.java
+++ src/test/java/be/vinci/pae/ucc/ViewContactUCCTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
+import be.vinci.pae.business.domain.ContactDTO;
 import be.vinci.pae.business.domain.DomainFactory;
-import be.vinci.pae.business.domain.ViewContactDTO;
 import be.vinci.pae.business.ucc.ViewContactUCC;
 import be.vinci.pae.dal.ViewContactDAO;
 import be.vinci.pae.utils.AppBinderTest;
@@ -24,7 +24,7 @@ public class ViewContactUCCTest {
   private DomainFactory domainFactory;
   @Mock
   private ViewContactDAO contactDAO;
-  private ArrayList<ViewContactDTO> expectedContacts;
+  private ArrayList<ContactDTO> expectedContacts;
 
   /**
    * Method executed before each test.
@@ -45,23 +45,23 @@ public class ViewContactUCCTest {
   @Test
   public void testGetContactsByUserId() {
 
-    ViewContactDTO expectedContact1 = domainFactory.getContact();
+    ContactDTO expectedContact1 = domainFactory.getContact();
     expectedContact1.setId(4);
     expectedContacts.add(expectedContact1);
-    ViewContactDTO expectedContact2 = domainFactory.getContact();
+    ContactDTO expectedContact2 = domainFactory.getContact();
     expectedContact2.setId(5);
     expectedContacts.add(expectedContact2);
-    ViewContactDTO expectedContact3 = domainFactory.getContact();
+    ContactDTO expectedContact3 = domainFactory.getContact();
     expectedContact3.setId(6);
     expectedContacts.add(expectedContact3);
-    ViewContactDTO expectedContact4 = domainFactory.getContact();
+    ContactDTO expectedContact4 = domainFactory.getContact();
     expectedContact4.setId(7);
     expectedContacts.add(expectedContact4);
 
     int userId = 7;
     when(contactDAO.getContactsByUserId(userId)).thenReturn(expectedContacts);
 
-    ArrayList<ViewContactDTO> result = contactUCC.getContactsByUserId(userId);
+    ArrayList<ContactDTO> result = contactUCC.getContactsByUserId(userId);
 
     assertNotNull(result);
     assertEquals(expectedContact1, result.get(0));
@@ -76,14 +76,14 @@ public class ViewContactUCCTest {
   @Test
   public void testGetTakenContactsByUserId() {
 
-    ViewContactDTO expectedContact = domainFactory.getContact();
+    ContactDTO expectedContact = domainFactory.getContact();
     expectedContact.setId(8);
     expectedContacts.add(expectedContact);
 
     int userId = 5;
     when(contactDAO.getContactsByUserId(userId)).thenReturn(expectedContacts);
 
-    ArrayList<ViewContactDTO> result = contactUCC.getContactsByUserId(userId);
+    ArrayList<ContactDTO> result = contactUCC.getContactsByUserId(userId);
 
     assertNotNull(result);
     assertEquals(expectedContact, result.get(0));
