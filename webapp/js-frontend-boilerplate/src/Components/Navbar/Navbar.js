6bf1c87fc52fa3e910cc6d915e36d8a7af92c0ef
mona-naitmazi <mona.naitmazi@student.vinci.be>
2024-03-25 22:08:05 +0100

    refactor: trying to modify contacts data

--- webapp/js-frontend-boilerplate/src/Components/Navbar/Navbar.js
+++ webapp/js-frontend-boilerplate/src/Components/Navbar/Navbar.js
@@ -30,10 +30,7 @@ function renderNavbar() {
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">      
             <li id="loginItem" class="nav-item">
               <a class="nav-link" href="#" data-uri="/login" style="color: white;">Se connecter</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" data-uri="/meetCompany" style="color: white;">Indiquer une rencontre avec l'entreprise</a>
-            </li>      
+            </li>     
             <li class="nav-item">
               <a class="nav-link" href="#" data-uri="/companyRefused" style="color: white;">entreprise refuse le stage</a>
             </li>
