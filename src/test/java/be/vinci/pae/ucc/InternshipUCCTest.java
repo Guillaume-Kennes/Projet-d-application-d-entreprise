35a12b54e8fbd05105a0d8cdd99608817bf8269a
mona-naitmazi <mona.naitmazi@student.vinci.be>
2024-03-25 16:30:21 +0100

    feature: attempt at contact test

--- src/test/java/be/vinci/pae/ucc/InternshipUCCTest.java
+++ src/test/java/be/vinci/pae/ucc/InternshipUCCTest.java
@@ -3,7 +3,6 @@ package be.vinci.pae.ucc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import be.vinci.pae.business.domain.DomainFactory;
