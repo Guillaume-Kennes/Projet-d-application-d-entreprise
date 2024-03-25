e397a494cc84c4aa67748f2cb252a0babbce25a9
mona-naitmazi <mona.naitmazi@student.vinci.be>
2024-03-25 14:52:43 +0100

    feature: attempt at contact test

---
+++ src/test/java/be/vinci/pae/ucc/ViewContactUCCTest.java
@@ -0,0 +1,72 @@
+package be.vinci.pae.ucc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import be.vinci.pae.business.domain.DomainFactory;
+import be.vinci.pae.business.domain.ViewContactDTO;
+import be.vinci.pae.business.ucc.ViewContactUCC;
+import be.vinci.pae.dal.ViewContactDAO;
+import be.vinci.pae.utils.AppBinderTest;
+import java.util.ArrayList;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+/**
+ * Test class for ViewContactUCC.
+ */
+public class ViewContactUCCTest {
+  private ViewContactUCC contactUCC;
+  private DomainFactory domainFactory;
+  @Mock
+  private ViewContactDAO contactDAO;
+  private ArrayList<ViewContactDTO> expectedContacts;
+
+  /**
+   * Method executed before each test.
+   */
+  @BeforeEach
+  public void setUp() {
+    ServiceLocator locator = ServiceLocatorUtilities.bind(new AppBinderTest());
+
+    contactUCC = locator.getService(ViewContactUCC.class);
+    contactDAO = locator.getService(ViewContactDAO.class);
+    domainFactory = locator.getService(DomainFactory.class);
+    expectedContacts = new ArrayList<>();
+  }
+
+  /**
+   * Test for successful retrieving of the user's contacts.
+   */
+  @Test
+  public void testGetContactsByUserId() {
+    int userId = 7;
+
+    ViewContactDTO expectedContact1 = domainFactory.getContact();
+    expectedContact1.setId(4);
+    expectedContacts.add(expectedContact1);
+    ViewContactDTO expectedContact2 = domainFactory.getContact();
+    expectedContact2.setId(5);
+    expectedContacts.add(expectedContact2);
+    ViewContactDTO expectedContact3 = domainFactory.getContact();
+    expectedContact3.setId(6);
+    expectedContacts.add(expectedContact3);
+    ViewContactDTO expectedContact4 = domainFactory.getContact();
+    expectedContact4.setId(7);
+    expectedContacts.add(expectedContact4);
+
+    when(contactDAO.getContactsByUserId(userId)).thenReturn(expectedContacts);
+
+    ArrayList<ViewContactDTO> result = contactUCC.getContactsByUserId(userId);
+
+    assertNotNull(result);
+    assertEquals(expectedContact1, result.get(0));
+    assertEquals(expectedContact2, result.get(1));
+    assertEquals(expectedContact3, result.get(2));
+    assertEquals(expectedContact4, result.get(3));
+  }
+}
--- src/test/java/be/vinci/pae/utils/AppBinderTest.java
+++ src/test/java/be/vinci/pae/utils/AppBinderTest.java
@@ -6,6 +6,8 @@ import be.vinci.pae.business.ucc.InternshipUCC;
 import be.vinci.pae.business.ucc.InternshipUCCImpl;
 import be.vinci.pae.business.ucc.UserUCC;
 import be.vinci.pae.business.ucc.UserUCCImpl;
+import be.vinci.pae.business.ucc.ViewContactUCC;
+import be.vinci.pae.business.ucc.ViewContactUCCImpl;
 import be.vinci.pae.dal.DALBackServices;
 import be.vinci.pae.dal.DALServices;
 import be.vinci.pae.dal.DALServicesImpl;
@@ -13,6 +15,8 @@ import be.vinci.pae.dal.InternshipDAO;
 import be.vinci.pae.dal.InternshipDAOImpl;
 import be.vinci.pae.dal.UserDAO;
 import be.vinci.pae.dal.UserDAOImpl;
+import be.vinci.pae.dal.ViewContactDAO;
+import be.vinci.pae.dal.ViewContactDAOImpl;
 import jakarta.ws.rs.ext.Provider;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.mockito.Mockito;
@@ -36,5 +40,7 @@ public class AppBinderTest extends AbstractBinder {
     bind(DALServicesImpl.class).to(DALBackServices.class).to(DALServices.class);
     bind(InternshipUCCImpl.class).to(InternshipUCC.class);
     bind(Mockito.mock(InternshipDAOImpl.class)).to(InternshipDAO.class);
+    bind(ViewContactUCCImpl.class).to(ViewContactUCC.class);
+    bind(Mockito.mock(ViewContactDAOImpl.class)).to(ViewContactDAO.class);
   }
 }
