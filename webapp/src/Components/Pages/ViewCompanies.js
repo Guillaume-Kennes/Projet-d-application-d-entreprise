2261f1bb229790709c0d33534b128269e29426a5
KaayTx <kawtar.dahman@student.vinci.be>
2024-04-12 21:51:55 +0100

    éfeat: add sorting function in ViewCompanies.js

--- webapp/src/Components/Pages/ViewCompanies.js
+++ webapp/src/Components/Pages/ViewCompanies.js
@@ -8,25 +8,22 @@ const viewAllCompaniesPage = async () => {
 
 }
 
-async function fetchCompanies(sort) {
+async function fetchCompanies() {
   const token = getToken();
-    const options = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: token
-      },
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token
+    },
   };
-    let url = "http://localhost:3000/companies/getEnterprises";
-    if (sort) {
-      url += `?sort=${sort}`;
-    }
+  const url = "http://localhost:3000/companies/getEnterprises";
   const response = await fetch(url, options);
 
   if (!response.ok) throw new Error(
       `fetch error : ${response.status} : ${response.statusText}`)
 
-  return await response.json();
+  return response.json();
 }
 
 async function allCompanies(companies) {
@@ -36,10 +33,10 @@ async function allCompanies(companies) {
           <thead>
             <tr>
               <th scope="col">ID</th>
-              <th scope="col">Trade Name <button class="sort-button" data-column="tradeName" value="tradeName">&#x25BC;</button><button class="sort-button" data-column="tradeName" value="-tradeName">&#x25B2;</button></th>
-              <th scope="col">Address <button class="sort-button" data-column="address" value="address">&#x25BC;</button><button class="sort-button" data-column="address" value="-address">&#x25B2;</button></th>
-              <th scope="col">City <button class="sort-button" data-column="city" value="city">&#x25BC;</button><button class="sort-button" data-column="city" value="-city">&#x25B2;</button></th>
-              <th scope="col">Means of communication <button class="sort-button" data-column="meansOfCommunication" value="communication">&#x25BC;</button><button class="sort-button" data-column="meansOfCommunication" value="-communication">&#x25B2;</button></th>
+              <th scope="col">Nom <button class="sort-button" data-column="tradeName" value="tradeName">&#x25BC;</button><button class="sort-button" data-column="tradeName" value="-tradeName">&#x25B2;</button></th>
+              <th scope="col">Adresse <button class="sort-button" data-column="address" value="address">&#x25BC;</button><button class="sort-button" data-column="address" value="-address">&#x25B2;</button></th>
+              <th scope="col">Ville <button class="sort-button" data-column="city" value="city">&#x25BC;</button><button class="sort-button" data-column="city" value="-city">&#x25B2;</button></th>
+              <th scope="col">Moyen de communication <button class="sort-button" data-column="meansOfCommunication" value="communication">&#x25BC;</button><button class="sort-button" data-column="meansOfCommunication" value="-communication">&#x25B2;</button></th>
             </tr>
           </thead>
           <tbody>
@@ -56,18 +53,26 @@ async function addListeners() {
   // Écoutez les événements de clic sur les boutons de tri
   main.querySelectorAll('.sort-button').forEach(button => {
     button.addEventListener('click', async () => {
-      const sort = button.value; // Récupérer la valeur du bouton de tri
-      console.log(sort);
-
-      const companies = await fetchCompanies({sort: sort});
+      const sortColumn = button.dataset.column;
+      const sortOrder = button.value.startsWith('-') ? 'desc' : 'asc';
+      let companies = await fetchCompanies();
+      // Trier les entreprises
+      companies = sortCompanies(companies, sortColumn, sortOrder);
       // Rendre les entreprises triées
-      // TODO API TRI DES ENTREPRISES
-
       await allCompanies(companies);
     });
   });
 }
 
+function sortCompanies(companies, sortColumn, sortOrder) {
+  return companies.sort((a, b) => {
+    if (sortOrder === 'asc') {
+      return a[sortColumn] > b[sortColumn] ? 1 : -1;
+    }
+      return a[sortColumn] < b[sortColumn] ? 1 : -1;
+
+  });
+}
 
 function setCompanyRow(companies) {
   const body = document.querySelector("tbody");
