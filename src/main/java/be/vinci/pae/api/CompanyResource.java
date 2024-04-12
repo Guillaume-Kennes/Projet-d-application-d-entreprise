194b3fc700f001d2d7172a15266daf0b2972d601
KaayTx <kawtar.dahman@student.vinci.be>
2024-04-12 21:59:11 +0100

    fix: fixing code to pass pmd and checkstyle on Jenkins

--- src/main/java/be/vinci/pae/api/CompanyResource.java
+++ src/main/java/be/vinci/pae/api/CompanyResource.java
@@ -2,16 +2,12 @@ package be.vinci.pae.api;
 
 import be.vinci.pae.api.filters.Authorize;
 import be.vinci.pae.business.domain.CompanyDTO;
-import be.vinci.pae.business.domain.UserDTO;
 import be.vinci.pae.business.ucc.CompanyUCC;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.container.ContainerRequestContext;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -35,8 +31,6 @@ public class CompanyResource {
   @Produces(MediaType.APPLICATION_JSON)
   @Authorize (value = {"Professeur"})
   public List<CompanyDTO> getAllEnterprises() {
-//    UserDTO authentificatedUser = (UserDTO) requestContext.getProperty("user");
-//    System.out.println(authentificatedUser); //juste pour Jenkins
     return companyUCC.getAllEnterprises();
   }
 }
--- src/test/java/be/vinci/pae/ucc/CompanyUCCTest.java
+++ src/test/java/be/vinci/pae/ucc/CompanyUCCTest.java
@@ -1,22 +1,18 @@
 package be.vinci.pae.ucc;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
-
 import be.vinci.pae.business.domain.CompanyDTO;
 import be.vinci.pae.business.domain.DomainFactory;
 import be.vinci.pae.business.ucc.CompanyUCC;
 import be.vinci.pae.dal.CompanyDAO;
 import be.vinci.pae.utils.AppBinderTest;
-import be.vinci.pae.utils.exception.BusinessException;
-import java.util.ArrayList;
-import java.util.List;
+
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
+/**
+ * The type Company ucc test.
+ */
 public class CompanyUCCTest {
   private CompanyUCC companyUCC;
   private CompanyDAO companyDAO;
