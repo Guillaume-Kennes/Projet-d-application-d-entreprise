d36ffb5967be14ec27e3dcc9e3516d7ddee2fc4a
KaayTx <kawtar.dahman@student.vinci.be>
2024-04-12 16:30:31 +0100

    refactor: add functions to sort in frontend

--- webapp/src/Components/Pages/ViewCompanies.js
+++ webapp/src/Components/Pages/ViewCompanies.js
@@ -1,12 +1,12 @@
-import {clearPage} from "../../utils/render";
-import {getToken} from "../../utils/user";
+import { clearPage } from "../../utils/render";
+import { getToken } from "../../utils/user";
 
 const viewAllCompaniesPage = async () => {
   clearPage();
   await allCompanies();
 }
 
-async function allCompanies() {
+async function fetchCompanies(options) {
   const token = getToken();
 
   const main = document.querySelector('main');
@@ -25,42 +25,75 @@ async function allCompanies() {
   try {
     if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`)
 
-    const companies = await response.json();
+  return await response.json();
+}
 
-    const companyRows = companies.map(company => `
-          <tr>
+async function allCompanies(companies) {
+  const main = document.querySelector('main');
+  main.innerHTML = `
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Trade Name <button class="sort-button" data-column="tradeName" value="tradeName">&#x25BC;</button><button class="sort-button" data-column="tradeName" value="-tradeName">&#x25B2;</button></th>
+              <th scope="col">Address <button class="sort-button" data-column="address" value="address">&#x25BC;</button><button class="sort-button" data-column="address" value="-address">&#x25B2;</button></th>
+              <th scope="col">City <button class="sort-button" data-column="city" value="city">&#x25BC;</button><button class="sort-button" data-column="city" value="-city">&#x25B2;</button></th>
+              <th scope="col">Means of communication <button class="sort-button" data-column="meansOfCommunication" value="communication">&#x25BC;</button><button class="sort-button" data-column="meansOfCommunication" value="-communication">&#x25B2;</button></th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      `;
+  setCompanyRow(companies);
+  await addListeners();
+}
+
+
+async function addListeners() {
+  const main = document.querySelector("main");
+  // Écoutez les événements de clic sur les boutons de tri
+  main.querySelectorAll('.sort-button').forEach(button => {
+    button.addEventListener('click', async () => {
+      const sort = button.value; // Récupérer la valeur du bouton de tri
+      console.log(sort);
+      const options = {
+        method: 'GET',
+        body: JSON.stringify({
+          sort,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: getToken(),
+        },
+      };
+
+      const companies = await fetchCompanies(options);
+      // Rendre les entreprises triées
+      // TODO API TRI DES ENTREPRISES
+
+      allCompanies(companies);
+    });
+  });
+}
+
+
+function setCompanyRow(companies) {
+  const body = document.querySelector("tbody");
+  companies.forEach(company =>  {
+
+    body.innerHTML += `
+      <tr>
             <td>${company.id}</td>
             <td>${company.tradeName}</td>
-            <td>${company.designation}</td>
-            <td>${company.address}</td>
-            <td>${company.city}</td>
-            <td>${company.meansOfCommunication}</td>
-          </tr>
-        `);
+             <td >${company.address}</td>
+             <td >${company.city}</td>
+             <td >${company.meansOfCommunication || ''}</td>
+        
+      </tr>
+    `;
+  });
 
-    main.innerHTML = `
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Trade Name</th>
-                <th scope="col">Designation</th>
-                <th scope="col">Address</th>
-                <th scope="col">City</th>
-                <th scope="col">Means of communication</th>
-              </tr>
-            </thead>
-            <tbody>
-              ${companyRows.join('')}
-            </tbody>
-          </table>
-        `;
-  } catch (error) {
-    // Gestion des erreurs existante
-    alert(
-        'Vous ne possédez pas les droits pour accéder à cette ressource. Seulement les professeurs ou administratifs peuvent y accéder');
-    console.error('Une erreur est survenue : ', error);
-  }
 }
 
 export default viewAllCompaniesPage;
