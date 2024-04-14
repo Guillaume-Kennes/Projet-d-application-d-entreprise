dc6287a3e8c1f1bcafb0756d2ef94ab507087b36
KaayTx <kawtar.dahman@student.vinci.be>
2024-04-15 00:49:54 +0100

    feat: modify getStudentsWithoutInternship in UserDAOImpl

--- src/main/java/be/vinci/pae/dal/UserDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/UserDAOImpl.java
@@ -233,11 +233,18 @@ public class UserDAOImpl implements UserDAO {
         + "    FROM pae.inscriptions_ue iue "
         + "    JOIN pae.contacts c ON iue.id_inscription_ue = c.inscription_ue "
         + "    JOIN pae.internships i ON c.id_contact = i.contact "
+        + "    WHERE iue.school_year = ? "
+        + ")"
+        + "AND u.id_user IN ("
+        + "    SELECT DISTINCT student "
+        + "    FROM pae.inscriptions_ue "
+        + "    WHERE school_year = ? "
         + ")";
     System.out.println("QUERY = " + query);
     try (PreparedStatement preparedStatement = dalServices.getPreparedStatement(query)) {
       preparedStatement.setString(1, schoolYear);
       preparedStatement.setString(2, schoolYear);
+
       System.out.println("PREPARED STATEMENT = " + preparedStatement);
       System.out.println("SCHOOL YEAR = " + schoolYear);
       try (ResultSet resultSet = preparedStatement.executeQuery()) {
