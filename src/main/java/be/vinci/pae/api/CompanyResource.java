97d439c28f9859400fb3740b15184d6a457ae553
KaayTx <kawtar.dahman@student.vinci.be>
2024-04-18 15:15:37 +0100

    feat: add javadoc for CompanyResource

--- src/main/java/be/vinci/pae/api/CompanyResource.java
+++ src/main/java/be/vinci/pae/api/CompanyResource.java
@@ -76,6 +76,12 @@ public class CompanyResource {
     }
   }
 
+  /**
+   * Get company by id.
+   *
+   * @param idCompany the id of the company
+   * @return the company by id
+   */
   @GET
   @Path("/numberOfStudentsTaken/{idCompany}")
   @Produces(MediaType.APPLICATION_JSON)
