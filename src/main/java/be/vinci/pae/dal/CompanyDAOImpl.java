776b6d65c4c0664564e0486455322ad3de69463d
mona-naitmazi <mona.naitmazi@student.vinci.be>
2024-04-23 20:46:10 +0200

    fix: making the profile work with the internship

--- src/main/java/be/vinci/pae/dal/CompanyDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/CompanyDAOImpl.java
@@ -129,7 +129,6 @@ public class CompanyDAOImpl implements CompanyDAO {
   public List<CompanyDTO> getAllEnterprises() {
     List<CompanyDTO> enterprisesList = new ArrayList<>();
 
-
     PreparedStatement preparedStatement = dalServices.getPreparedStatement(
         "SELECT * FROM pae.enterprises ORDER BY trade_name, designation");
     try (ResultSet resultSet = preparedStatement.executeQuery()) {
