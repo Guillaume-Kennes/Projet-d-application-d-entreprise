8559f90abdd6425981917d674fae0b10fab06661
mona-naitmazi <mona.naitmazi@student.vinci.be>
2024-05-03 11:21:34 +0200

    fix: improving front end for contact-related operations

--- webapp/src/Components/Pages/ContactsPage.js
+++ webapp/src/Components/Pages/ContactsPage.js
@@ -37,11 +37,11 @@ function renderContactsPage(contact) {
       button2.className = 'btn btn-primary btn-block btn-light myButton';
       button3.className = 'btn btn-primary btn-block btn-light myButton';
 
-      contactText.textContent = `${description}`; // Display the contact description
+      contactText.textContent = `${description}`;
       button.textContent = "Indiquer que le contact est pris";
-      button.addEventListener("click", () => showForm(id)); // Attach event listener
+      button.addEventListener("click", () => Navigate(`/meetCompany?contactId=${id}`));
       button2.textContent = "Indiquer que le contact est refusé";
-      button2.addEventListener("click", () => showFormRefusal(id));
+      button2.addEventListener("click", () => Navigate(`/companyRefused?contactId=${id}`));
       button3.textContent = "Arrêter de suivre le contact";
       button3.addEventListener("click", () => stopFollowing(id));
       button4.textContent = "Créer un stage";
@@ -56,107 +56,6 @@ function renderContactsPage(contact) {
     });
   } else {
     contactList.innerHTML = "<li>Aucun contact</li>";
-  }
-}
-
-function showForm(idContact) {
-  const main = document.querySelector('main');
-  main.innerHTML = `
-    <div class="container mt-5">
-      <h1>Formulaire de Contact</h1>
-      <form id="meet-company-form">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="contactMade">
-          <label class="form-check-label" for="contactMade">
-            Contact pris
-          </label>
-        </div>
-        <br>
-        <h3> Lieu de rencontre avec l'entreprise </h3>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="meetingLocation" id="entreprise" value="entreprise">
-          <label class="form-check-label" for="entreprise">
-            Rencontre dans l'entreprise
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="meetingLocation" id="distance" value="distance">
-          <label class="form-check-label" for="distance">
-            Rencontre à distance
-          </label>
-        </div>
-        <button type="submit" class="btn btn-primary mt-3">Envoyer</button>
-      </form>
-    </div>
-  `;
-
-  const form = document.getElementById('meet-company-form');
-  form.addEventListener('submit', (e) => meetCompany(e, idContact));
-}
-
-function showFormRefusal(idContact) {
-  const main = document.querySelector('main');
-  main.innerHTML = `
-    <div class="container mt-5">
-      <h1>Formulaire de refus d'un contact</h1>
-      <form id="refusal-form">
-        <h3> Raisons du refus </h3>
-        <div class="form-group">
-          <label for="reason1">Raison :</label>
-            <input type="text" class="form-control" id="reason" name="reason" placeholder="Entrez la raison du refus" required>
-        </div>
-        <button type="submit" class="btn btn-primary mt-3">Envoyer</button>
-      </form>
-    </div>
-  `;
-  const form = document.getElementById('refusal-form');
-  form.addEventListener('submit', (e) => refuseInternship(e, idContact));
-}
-async function meetCompany(e, idContact) {
-  e.preventDefault();
-  const meetLocation = document.querySelector('input[name="meetingLocation"]:checked').value;
-  const token = getToken();
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: token,
-    },
-    body: JSON.stringify({ meetLocation }),
-  };
-
-  try {
-    const response = await fetch(`http://localhost:3000/contacts/meet/${idContact}`, options);
-    if (!response.ok) {
-      throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
-    }
-    // window.location.href = `ContactsPage?id=${getUserIdFromToken()}`;
-  } catch (error) {
-    console.error('Error meeting company:', error);
-  }
-}
-
-async function refuseInternship(e, idContact) {
-  e.preventDefault();
-  const reasonRefusal = document.querySelector('input[name="reason"]').value;
-  const token = getToken();
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: token,
-    },
-    body: JSON.stringify({ reasonRefusal }),
-  };
-
-  try {
-    const response = await fetch(`http://localhost:3000/contacts/companyrefused/${idContact}`, options);
-    if (!response.ok) {
-      throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
-    }
-    // window.location.href = `ContactsPage?id=${getUserIdFromToken()}`;
-  } catch (error) {
-    console.error('Error setting refusal reason :', error);
   }
 }
 
@@ -175,11 +74,12 @@ async function stopFollowing(idContact) {
     if (!response.ok) {
       throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
     }
-    // window.location.href = `ContactsPage?id=${getUserIdFromToken()}`;
+    Navigate(`/contacts?contactId=${idContact}`);
   } catch (error) {
     console.error('Error stopping following the contact :', error);
   }
 }
+
 async function getValues() {
   const authenticatedUser = getAuthenticatedUser();
   console.log("CONTACTS --> authenticatedUser : ", authenticatedUser);
