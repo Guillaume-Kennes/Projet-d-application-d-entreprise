c6087d27bdd61521c5a65c4d7934ae0fda5d26ee
chuqi-zhang-vinci <chuqi.zhang@student.vinci.be>
2024-05-08 11:40:42 +0200

    fix: internshipSupervisorImpl

--- src/main/java/be/vinci/pae/business/domain/InternshipSupervisorDTO.java
+++ src/main/java/be/vinci/pae/business/domain/InternshipSupervisorDTO.java
@@ -106,4 +106,32 @@ public interface InternshipSupervisorDTO {
    * @param versionNumber the version number to set
    */
   void setVersionNumber(int versionNumber);
+
+  /**
+   * Returns the trade name of the supervisor's company.
+   *
+   * @return the trade name of the supervisor's company
+   */
+  public String getTradeName();
+
+  /**
+   * Sets the trade name of the supervisor's company.
+   *
+   * @param tradeName the trade name to set
+   */
+  public void setTradeName(String tradeName);
+
+  /**
+   * Returns the designation of the supervisor.
+   *
+   * @return the designation of the supervisor
+   */
+  public String getDesignation();
+
+  /**
+   * Sets the designation of the supervisor.
+   *
+   * @param designation the designation to set
+   */
+  public void setDesignation(String designation);
 }
